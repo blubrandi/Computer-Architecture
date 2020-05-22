3a4ae97be30b3b237575bb7b01fdd3d23da1ceb8
Brand <blubrandi0@gmail.com>
2020-05-21 21:19:46 -0600

    Added stack, tests pass

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -4,21 +4,32 @@ HLT = 0b00000001
 LDI = 0b10000010
 PRN = 0b01000111
 MUL = 0b10100010
+POP = 0b01000110
+PUSH = 0b01000101
+ST = 0b10000100
 
 class CPU:
     """Main CPU class."""
-
     def __init__(self):
         self.reg = [0] * 8
         self.ram = [0] * 256
         self.pc = 0
-
+        self.sp = 7
+        self.value = 0
         self.branch_table = {}
-
         self.branch_table[HLT] = self.handle_hlt
         self.branch_table[LDI] = self.handle_ldi
         self.branch_table[PRN] = self.handle_prn
         self.branch_table[MUL] = self.handle_mul
+        self.branch_table[POP] = self.handle_pop
+        self.branch_table[PUSH] = self.handle_push
+        self.branch_table[ST] = self.handle_st
+
+    def handle_st(self, pc):
+        reg_num = self.ram[self.pc + 1]
+        value = self.ram[self.pc + 2]
+        self.reg[reg_num] = value
+        self.pc += 3
 
     def ram_read(self, MAR):
         return self.ram[MAR]
@@ -29,7 +40,6 @@ class CPU:
     def load(self):
         """Load a program into memory."""
         address = 0
-
         with open(sys.argv[1]) as f:
             for line in f:
                 string_val = line.split("#")[0].strip()
@@ -67,104 +77,58 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
         print()
 
+    def handle_push(self, reg_a, reg_b):
+        self.reg[self.sp] -= 1
+        reg_num = self.ram[self.pc + 1]
+        self.value = self.reg[reg_num]
+        self.ram[self.reg[self.sp]] = self.value
+        self.pc += 2
+
+    def handle_pop(self, reg_a):
+        top_of_stack_addr = self.reg[self.sp]
+        self.value = self.ram[top_of_stack_addr]
+        reg_num = self.ram[self.pc + 1]
+        self.reg[reg_num] = self.value
+        self.reg[self.sp] += 1
+        self.pc += 2
+
     def handle_hlt(self):
-        print("***** HALT COMMAND INITIATED ******")
         running = False
         self.pc += 1
         sys.exit()
 
     def handle_ldi(self, reg_a, reg_b):
-        print("******** LDI COMMAND INITIATED *******")
         self.reg[reg_a] = reg_b
-        print(self.reg[reg_a], "--- After")
         self.pc += 3
-        print(self.reg, "--- self.reg")
 
     def handle_prn(self, reg_a):
-        print("******* PRINT COMMAND INITIATED *********")
         print(self.reg[reg_a])
         self.pc += 2
 
     def handle_mul(self, reg_a, reg_b):
-        print("******** MULTIPLY COMMAND INITIATED ********")
         self.alu("MUL", reg_a, reg_b)
         self.pc += 3
 
     def run(self):
         """Run the CPU."""
         running = True
-       
-        while running == True:
 
+        while running == True:
             instruction = self.ram_read(self.pc)
             reg_a = self.ram_read(self.pc + 1)
             reg_b = self.ram_read(self.pc + 2)
-
             if instruction == LDI:
                 self.branch_table[instruction](reg_a, reg_b)
-                print(reg_a, reg_b)
-
             elif instruction == MUL:
                 self.branch_table[instruction](reg_a, reg_b)
-
             elif instruction == PRN:
                 self.branch_table[instruction](reg_a)
-
+            elif instruction == PUSH:
+                self.branch_table[instruction](reg_a, reg_b)
+            elif instruction == POP:
+                self.branch_table[instruction](reg_a)
             else:
                 self.branch_table[instruction]()
 
-            # instruction = LDI
-            # self.branch_table[instruction](reg_a, reg_b)
-
-            # instruction = MUL
-            # self.branch_table[instruction](reg_a, reg_b)
-
-            # instruction = PRN
-            # self.branch_table[instruction](reg_a)
-
-            # instruction = HLT
-            # self.branch_table[instruction]()
-
-
         running = False
-    
         sys.exit()
-        '''while running == True:
-            instruction = self.ram_read(self.pc)
-            reg_a = self.ram_read(self.pc + 1)
-            reg_b = self.ram_read(self.pc + 2)
-            if instruction == HLT:
-                print("***** HALT COMMAND INITIATED ******")
-                running = False
-                self.pc += 1
-                sys.exit()
-            elif instruction == LDI:
-                print("******** LDI COMMAND INITIATED *******")
-                print(self.reg, "--- self.reg")
-                print(reg_a, "--- reg_a")
-                print(reg_b, "--- reg_b")
-                print(self.reg[reg_a], "--- Before")
-                self.reg[reg_a] = reg_b
-                print(self.reg[reg_a], "--- After")
-                self.pc += 3
-                print(self.reg, "--- self.reg")
-            elif instruction == PRN:
-                print("******* PRINT COMMAND INITIATED *********")
-                print(self.pc, "--- self.pc")
-                print(self.reg, "--- self.reg")
-                print(reg_a, "--- reg_a")
-                print(self.reg[reg_a])
-                self.pc += 2
-            elif instruction == MUL:
-                print("******** MULTIPLY COMMAND INITIATED ********")
-                print(self.reg, " --- self.reg")
-                print(self.reg[reg_a], " -- reg_a")
-                print(self.reg[reg_b], " -- reg_b")
-                self.alu("MUL", reg_a, reg_b)
-                mul = self.reg[reg_a] * self.reg[reg_b]
-                print(mul)
-                self.pc += 3
-            else:
-                #print(f'unknown instruction {instruction} at address {pc}')
-                running = False
-                sys.exit()'''
